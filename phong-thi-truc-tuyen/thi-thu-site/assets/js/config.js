7f02b97923c62f30484435cbd5bc7ad7fb2973e3
luckgeter-droid <luckgeter@gmail.com>
2026-08-28 20:29:32 +0700

    Update config.js

--- phong-thi-truc-tuyen/thi-thu-site/assets/js/config.js
+++ phong-thi-truc-tuyen/thi-thu-site/assets/js/config.js
@@ -3,6 +3,6 @@
  * Dán URL Web App bạn lấy được sau khi Deploy Google Apps Script vào đây.
  * URL có dạng: https://script.google.com/macros/s/XXXXXXXXXXXX/exec
  */
-const APPS_SCRIPT_URL = 'DÁN_URL_APPS_SCRIPT_CỦA_BẠN_VÀO_ĐÂY';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyhcx-sAuUBOtiwGhbPp_ExscJAj85xKaRira1MYI_gCDCXIl3_Y7D4-hNSkoCJfNbL/exec';
 
 window.CONFIG = { APPS_SCRIPT_URL };
